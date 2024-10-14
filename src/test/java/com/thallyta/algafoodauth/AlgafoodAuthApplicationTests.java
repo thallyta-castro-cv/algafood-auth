7dbcdbc9cc6d61150d3266823ccfd0c38d12737a
thallyta-castro-cv <thallyta180136319@gmail.com>
2024-10-13 21:12:06 -0300

    feat: criado Authorization Server para projeto Algafood API (Resource Server)

---
+++ src/test/java/com/thallyta/algafoodauth/AlgafoodAuthApplicationTests.java
@@ -0,0 +1,13 @@
+package com.thallyta.algafoodauth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlgafoodAuthApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
